ef29617e28478f0682517bc807b36fba017a1e56
Erik Björn <erik.g.bjoern@gmail.com>
2021-02-04 09:50:48 +0100

    add vue-apollo, apollo-boost, graphql

---
+++ apollo.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  client: {
+    service: {
+      name: "economin-api",
+      url: "http://localhost:3000/graphql"
+    }
+  }
+};
--- src/main.js
+++ src/main.js
@@ -4,11 +4,24 @@ import './registerServiceWorker'
 import router from './router'
 import store from './store'
 import './assets/styles/index.css'
+import ApolloClient from 'apollo-boost'
+import VueApollo from 'vue-apollo'
+
+const apolloClient = new ApolloClient({
+  uri: process.env.VUE_APP_SERVER_URL
+})
+
+Vue.use(VueApollo)
+
+const apolloProvider = new VueApollo({
+  defaultClient: apolloClient,
+})
 
 Vue.config.productionTip = false
 
 new Vue({
   router,
   store,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app')
