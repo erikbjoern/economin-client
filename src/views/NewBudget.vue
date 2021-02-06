d2a017e7abc272dc3ee966d9caec65c49c38d232
Erik Björn <erik.g.bjoern@gmail.com>
2021-02-06 22:02:08 +0100

    add 'saving' state during createBudget

--- src/views/NewBudget.vue
+++ src/views/NewBudget.vue
@@ -17,97 +17,102 @@
       </div>
       <div v-if="!$store.state.currentBudget.id" class="h-screen">
         <h1
-          class="text-center text-lg text-gray-200 font-semibold font-main mb-5"
+          class="text-center text-lg text-gray-200 font-semibold font-main"
         >
           Ny budget
         </h1>
-        <form @submit="createBudget" class="flex flex-col space-y-10 mt-8" ref="form">
-          <div class="formgrid grid grid-cols-auto-3 gap-2">
-            <label class="font-semibold text-gray-200 mb-6">Från:</label>
-            <functional-calendar
-              class="col-span-2"
-              id="startDay"
-              :is-date-picker="true"
-              :is-modal="true"
-              :transition="true"
-              :is-auto-closeable="true"
-              :is-typeable="true"
-              :is-layout-expandable="true"
-              :placeholder="today"
-              v-model="calendarData"
-            ></functional-calendar>
-            <label htmlFor="length" class="font-semibold text-gray-200"
-              >Längd:</label
-            >
-            <input
-              id="length"
-              type="number"
-              class="w-14 rounded-sm text-gray-600 text-right pr-px shadow"
-              v-model="length"
-            />
-            <select
-              name="unit"
-              id="unit"
-              class="rounded-sm text-gray-600 shadow"
-              v-model="unit"
-            >
-              <option value="DAYS">{{ dayOrDays(length) }}</option>
-              <option value="MONTHS">{{ monthOrMonths(length) }} </option>
-            </select>
-            <div
-              class="pointer-events-none col-span-full max-h-0 transition-min-max-h duration-400 text-white w-56 bg-red-900 opacity-80 rounded-lg mx-auto shadow"
-              :class="{ 'max-h-56': errors.dateError }"
-            >
-              <p
-                class="p-5 opacity-0 transition-opacity duration-400"
-                :class="{ 'opacity-100': errors.dateError }"
+        <form @submit="createBudget" class="flex flex-col space-y-10">
+          <div
+            class="flex flex-col space-y-10 mt-8 transition-opacity"
+            :class="{ 'opacity-40': isSaving }"
+          >
+            <div class="formgrid grid grid-cols-auto-3 gap-2">
+              <label class="font-semibold text-gray-200 mb-6">Från:</label>
+              <functional-calendar
+                class="col-span-2"
+                id="startDay"
+                :is-date-picker="true"
+                :is-modal="true"
+                :transition="true"
+                :is-auto-closeable="true"
+                :is-typeable="true"
+                :is-layout-expandable="true"
+                :placeholder="today"
+                v-model="calendarData"
+              ></functional-calendar>
+              <label htmlFor="length" class="font-semibold text-gray-200"
+                >Längd:</label
               >
-                {{ errors.dateError || ":)" }}
-              </p>
-            </div>
-            <label htmlFor="amount" class="font-semibold text-gray-200"
-              >Summa:</label
-            >
-            <input
-              id="amount"
-              type="number"
-              class="w-14 rounded-sm text-gray-600 text-right pr-px shadow"
-              v-model="amount"
-              min="1"
-            />
-            <select
-              name="currency"
-              id="currency"
-              class="rounded-sm text-gray-600 shadow"
-            >
-              <option value="SEK">kr</option>
-              <option value="USD">$</option>
-              <option value="EUR">€</option>
-              <option value="GBP">£</option>
-              <option value="YEN">¥</option>
-            </select>
-            <div
-              class="pointer-events-none col-span-full max-h-0 transition-min-max-h duration-400 text-white opacity-80 w-56 bg-red-900 rounded-lg mx-auto shadow"
-              :class="{ 'max-h-56': errors.amountError }"
-            >
-              <p
-                class="p-5 opacity-0 transition-opacity duration-400"
-                :class="{ 'opacity-100': errors.amountError }"
+              <input
+                id="length"
+                type="number"
+                class="w-14 rounded-sm text-gray-600 text-right pr-px shadow"
+                v-model="length"
+              />
+              <select
+                name="unit"
+                id="unit"
+                class="rounded-sm text-gray-600 shadow"
+                v-model="unit"
               >
-                {{ errors.amountError || ":)" }}
-              </p>
+                <option value="DAYS">{{ dayOrDays(length) }}</option>
+                <option value="MONTHS">{{ monthOrMonths(length) }} </option>
+              </select>
+              <div
+                class="pointer-events-none col-span-full max-h-0 transition-min-max-h duration-400 text-white w-56 bg-red-900 opacity-80 rounded-lg mx-auto shadow"
+                :class="{ 'max-h-56': errors.dateError }"
+              >
+                <p
+                  class="p-5 opacity-0 transition-opacity duration-400"
+                  :class="{ 'opacity-100': errors.dateError }"
+                >
+                  {{ errors.dateError || ":)" }}
+                </p>
+              </div>
+              <label htmlFor="amount" class="font-semibold text-gray-200"
+                >Summa:</label
+              >
+              <input
+                id="amount"
+                type="number"
+                class="w-14 rounded-sm text-gray-600 text-right pr-px shadow"
+                v-model="amount"
+                min="1"
+              />
+              <select
+                name="currency"
+                id="currency"
+                class="rounded-sm text-gray-600 shadow"
+              >
+                <option value="SEK">kr</option>
+                <option value="USD">$</option>
+                <option value="EUR">€</option>
+                <option value="GBP">£</option>
+                <option value="YEN">¥</option>
+              </select>
+              <div
+                class="pointer-events-none col-span-full max-h-0 transition-min-max-h duration-400 text-white opacity-80 w-56 bg-red-900 rounded-lg mx-auto shadow"
+                :class="{ 'max-h-56': errors.amountError }"
+              >
+                <p
+                  class="p-5 opacity-0 transition-opacity duration-400"
+                  :class="{ 'opacity-100': errors.amountError }"
+                >
+                  {{ errors.amountError || ":)" }}
+                </p>
+              </div>
             </div>
-          </div>
-          <div class="flex items-center justify-center gap-3">
-            <input
-              id="recurring"
-              name="recurring"
-              type="checkbox"
-              class="shadow"
-            />
-            <label htmlFor="recurring" class="text-gray-200"
-              >Upprepa automatiskt</label
-            >
+            <div class="flex items-center justify-center gap-3">
+              <input
+                id="recurring"
+                name="recurring"
+                type="checkbox"
+                class="shadow"
+              />
+              <label htmlFor="recurring" class="text-gray-200"
+                >Upprepa automatiskt</label
+              >
+            </div>
           </div>
           <button
             type="submit"
@@ -117,7 +122,7 @@
               'pointer-events-none': hasAnyErrors,
             }"
           >
-            Spara
+            {{ isSaving ? "Sparar..." : "Spara" }}
           </button>
         </form>
       </div>
@@ -148,6 +153,7 @@ export default {
       currentYear: new Date().getFullYear(),
       errors: {},
       hasAnyErrors: false,
+      isSaving: false,
       length: 1,
       unit: "MONTHS",
     };
@@ -188,10 +194,14 @@ export default {
       event.preventDefault();
 
       if (this.hasAnyErrors) {
-        console.log("Returning from createBudget because of present errors, apparently")
-        return
-      };
-      
+        console.log(
+          "Returning from createBudget because of present errors, apparently"
+        );
+        return;
+      }
+
+      this.isSaving = true;
+
       try {
         const result = await this.$apollo.mutate({
           mutation: CREATE_BUDGET,
@@ -212,6 +222,8 @@ export default {
       } catch (error) {
         debugger;
       }
+
+      this.isSaving = false;
     },
     validateAmount() {
       if (this.amount <= 0) {
